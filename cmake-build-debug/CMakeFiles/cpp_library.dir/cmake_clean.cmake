f3393a28ce669a431b24236150eba952b8097d8c
TharindaNimnajith <tharindarajapakshe@gmail.com>
2021-10-15 23:06:55 +0530

    cpp library

---
+++ cmake-build-debug/CMakeFiles/cpp_library.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp_library.dir/library.cpp.obj"
+  "cpp_library.lib"
+  "cpp_library.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp_library.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
